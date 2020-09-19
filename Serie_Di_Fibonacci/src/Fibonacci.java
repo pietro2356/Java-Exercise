520146dbfaacb65cc5602e92445e899959ae2766
pietro2356 <pietro.rocchio@gmail.com>
2020-09-20 01:24:40 +0200

    Serie di fibonacci

---
+++ Serie_Di_Fibonacci/src/Fibonacci.java
@@ -0,0 +1,8 @@
+public class Fibonacci{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Serie di Fibonacci");
+        System.out.print("Imposta il valore di fine: ");
+
+        ///TODO: 
+    }
+}
